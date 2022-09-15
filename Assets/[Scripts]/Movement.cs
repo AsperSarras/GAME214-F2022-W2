b9d74b631a5e640de2407cd6c3800b41029b0deb
Asper Sarras <101324242@georgrebrown.ca>
2022-09-15 11:26:40 -0200

    Movement

--- Assets/[Scripts]/Movement.cs
+++ Assets/[Scripts]/Movement.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10;
+    public float speed = 10.0f;
+    public Boundry boundry;
+    public float verticalPos;
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.position = new Vector2(0.0f, verticalPos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        transform.position = new Vector2(transform.position.x + x,transform.position.y + y);
+        Move();
+        //CheckBounds();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        //transform.position += new Vector3(x, 0, 0);
+
+        transform.position += new Vector3(x, 0, 0);
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, boundry.min, boundry.max), verticalPos);
+        //Mathf.Clamp(x,boundry.min,boundry.max)
+        //transform.position = new Vector2(transform.position.x + x, 0);
+        //transform.position = new Vector2(transform.position.x + x,transform.position.y + y);
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.x > boundry.max)
+        {
+            transform.position = new Vector2(boundry.max, verticalPos);
+        }
+        if (transform.position.x < boundry.min)
+        {
+            transform.position = new Vector2(boundry.min, verticalPos);
+        }
     }
 }
